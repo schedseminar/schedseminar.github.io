8d4a5b9fe2c97114a5377c24d767ec810ccb2751
korlavik <korlavik@fel.cvut.cz>
2025-11-25 17:57:21 +0100

    site fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ViktorKorladinov.github.io',
+  site: 'https://schedulingseminar.com',
   integrations: [tailwind()],
   typescript: {
     strict: false,
